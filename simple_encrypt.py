1a67971d1a2580b7b87c0d1ecad046c7f1bd5af0
MrWatchman <joaooli17@hotmail.com>
2023-03-25 15:02:21 -0300

    primeiro teste encrypt

---
+++ simple_encrypt.py
@@ -0,0 +1,32 @@
+import random
+import string
+
+chars = " " + string.ascii_letters + string.digits + string.punctuation
+chars = list(chars)
+key = chars.copy()
+
+random.shuffle(key)
+
+# print(f"Original: {chars}")
+# print(f"Key: {key}")  
+
+def encrypt(text):
+    encrypted = ""
+    for char in text:
+        index = chars.index(char)
+        encrypted += key[index]
+    return encrypted
+
+def decrypt(text):
+    decrypted = ""
+    for char in text:
+        index = key.index(char)
+        decrypted += chars[index]
+    return decrypted
+
+text = input("Type a message: ")
+encrypted = encrypt(text)
+decrypted = decrypt(encrypted)
+
+print(f"Original: {text}")
+print(f"Encrypted: {encrypted}")
